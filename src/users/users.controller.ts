60e398647fe7eefd205704168cc3c4a66beaa0c9
jsh <jeong9260@naver.com>
2023-11-14 19:28:14 +0900

    [ADD]로그인 기능 구현

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { UserDto } from './dto/users.dto';
 import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
   @Post('/signup')
-  signUp(@Body() userDto: UserDto): Promise<void> {
+  signUp(@Body(ValidationPipe) userDto: UserDto): Promise<void> {
     return this.usersService.signUp(userDto);
+  }
+  @Post('/signin')
+  signIn(@Body() userDto: UserDto) {
+    return this.usersService.signIn(userDto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
 import { UserDto } from './dto/users.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
@@ -12,5 +13,15 @@ export class UsersService {
 
   async signUp(userDto: UserDto): Promise<void> {
     return this.usersRepository.createUser(userDto);
+  }
+  async signIn(userDto: UserDto): Promise<string> {
+    const { userid, password } = userDto;
+    const user = await this.usersRepository.findOne({ where: { userid } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return '로그인 성공';
+    } else {
+      throw new UnauthorizedException('로그인 실팬');
+    }
   }
 }
