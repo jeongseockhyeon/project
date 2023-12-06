c0e2db5c3c2feac5f877c7b69eb142c5c1f176b0
jsh <jeong9260@naver.com>
2023-12-06 20:14:09 +0900

    [Update]

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const serverConfig = config.get('server');
   app.use(cookieParser());
   await app.listen(serverConfig.port);
